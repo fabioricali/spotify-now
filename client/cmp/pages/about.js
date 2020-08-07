c522710fc80d45647b23fa62bc1cc39ede13a10e
Fabio Ricali <ricalifabio@gmail.com>
2020-08-07 10:13:23 +0200

    progress

--- client/cmp/pages/about.js
+++ client/cmp/pages/about.js
@@ -10,9 +10,13 @@ export default class extends Component {
         return h`
             <div>
                  <h1>${this.props.title}</h1>
-                 <p>This simple app shows the songs listened to simultaneously.<br>
-                 The data are taken from 
-                 <a style="color: white" href="https://listeningtogether.atspotify.com" target="_blank">listeningtogether.atspotify.com</a>.</p>
+                 <p>
+                     This simple app shows the songs listened to simultaneously.<br>
+                     The data are taken from 
+                     <a style="color: white" href="https://listeningtogether.atspotify.com" target="_blank">
+                        listeningtogether.atspotify.com
+                     </a>.
+                 </p>
             </div>
         `
     }
